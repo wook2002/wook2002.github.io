2cb62826d7351fb583f76f26941a6c4f09d33684
ejejej2002 <ejejej2002@gmail.com>
2022-12-17 18:58:08 +0900

    detail

--- src/components/board/FreeBoardDetail.vue
+++ src/components/board/FreeBoardDetail.vue
@@ -6,21 +6,37 @@
     <div><button>수정</button></div>
     <div><button>삭제</button></div> -->
     <p>-----------------</p>
-    <div>freeBoardData</div>
-    <div>writer</div>
-    <div>내용</div>
-    {{$route.params.id}}
+    <div>{{connectData.title}}</div> 
+    <div>{{connectData.writer}}</div>
+    <div>{{connectData.content}}</div>
+    <div>{{connectData.freeDate}}</div>
+    <div>{{connectData.recommend}}</div>
+    <div>{{connectData.lookup}}</div>
     
     <p>-----------------</p>
     <div><button>댓글</button></div>
     <div><button>추천해요</button></div>
     <div>[댓글창]</div>
   </div>
+<!-- 테스트
+  {{$route.params.id}} -->
+  {{connectData}}
 </template>
 
 <script>
 export default {
     name:"FreeBoardDetailA",
+    mounted: function () {
+        this.$axios.get("/freeBoard/detail/"+ this.connectData)
+          .then(response => {
+            this.connectData = response.data[0]
+          })
+    },
+    data() {
+      return {
+        connectData:this.$route.params.id,
+      }
+    },
     
     
 }
